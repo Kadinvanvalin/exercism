a928193ad755fdac566f46e76c7bfc4d0b883fea
Kadin <kadinvanvalin@yahoo.com>
2019-09-20 11:26:57 -0500

    first submission roman numerals

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -2,36 +2,34 @@ defmodule RomanNumerals do
   @doc """
   Convert the number to a roman number.
   """
+  @roman_nums [
+    {"M", 1000},
+    {"CM", 900},
+    {"D", 500},
+    {"CD", 400},
+    {"C", 100},
+    {"XC", 90},
+    {"L", 50},
+    {"XL", 40},
+    {"X", 10},
+    {"IX", 9},
+    {"V", 5},
+    {"IV", 4},
+    {"I", 1}
+  ]
+
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
-    convert("", number)
-  end
-  # romanNums = %{
-  #   "X": 10,
-
-  # }
-
-  def convert(roman, number) when number >= 10 do
-    (roman <> "X")
-    |> convert(number - 10)
+    meta_convert(@roman_nums, "", number);
   end
 
-  def convert(roman, number) when number >= 5  do
-    (roman <> "V")
-    |> convert(number - 5)
-  end
-  
-  def convert(roman, number) when number >= 4 do
-    (roman <> "IV")
-    |> convert(number - 4)
+  defp meta_convert([], roman, 0), do: roman
+ 
+  defp meta_convert([h|t], roman, number) when number >= elem(h, 1) do
+    meta_convert([h|t], roman <> elem(h, 0), number - elem(h, 1))
   end
 
-  def convert(roman, number) when number > 0 do
-    (roman <> "I")
-    |> convert(number - 1)
-  end
-
-  def convert(roman, 0) do
-    roman
+  defp meta_convert([_h|t], roman, number) do
+    meta_convert(t, roman, number)
   end
 end
