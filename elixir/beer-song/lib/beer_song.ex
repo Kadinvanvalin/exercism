89ce24c0290cb73bdf2e08e2a0290507ebf3a48f
Kadin <kadinvanvalin@yahoo.com>
2019-09-29 21:52:27 -0500

    submit beer song

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -2,16 +2,43 @@ defmodule BeerSong do
   @doc """
   Get a single verse of the beer song
   """
-  @spec verse(integer) :: String.t()
-  def verse(number) do
-    # Your implementation here...
+  @spec lyrics(Range.t()) :: String.t()
+  def lyrics(range \\ 99..0) do
+    Enum.map_join(range, "\n", &verse/1)
   end
 
-  @doc """
-  Get the entire beer song for a given range of numbers of bottles.
-  """
-  @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    # Your implementation here...
+  def verse(number) do
+    """
+    #{number |> bottleCount() |> String.capitalize()} of beer on the wall, #{bottleCount(number)} of beer.
+    #{callToAction(number)}, #{bottleCount(number - 1)} of beer on the wall.
+    """
+  end
+
+  defp callToAction(0) do
+    "Go to the store and buy some more"
+  end
+
+  defp callToAction(1) do
+    "Take it down and pass it around"
+  end
+
+  defp callToAction(_) do
+    "Take one down and pass it around"
+  end
+
+  defp bottleCount(0) do
+    "no more bottles"
+  end
+
+  defp bottleCount(1) do
+    "1 bottle"
+  end
+
+  defp bottleCount(-1) do
+    "99 bottles"
+  end
+
+  defp bottleCount(number) do
+    "#{number} bottles"
   end
 end
