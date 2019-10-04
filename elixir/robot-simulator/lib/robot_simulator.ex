292d4984e3efdf0479b5e1444291740266505b01
kadinvanvalin <kadin@Kadins-MBP.attlocal.net>
2019-10-04 07:48:44 -0500

    start robot sim

--- elixir/robot-simulator/lib/robot_simulator.ex
+++ elixir/robot-simulator/lib/robot_simulator.ex
@@ -5,9 +5,62 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(direction \\ :north, position \\ {0, 0}) do
+    cond do
+      not valid_direction?(direction) ->
+        {:error, "invalid direction"}
+      not valid_position?(position) ->
+        {:error, "invalid position"}
+      true ->
+         %{
+           position: position,
+           direction: direction
+         }
+    end
   end
 
+  defp valid_direction?(:north), do: true
+  defp valid_direction?(:east), do: true
+  defp valid_direction?(:south), do: true
+  defp valid_direction?(:west), do: true
+  defp valid_direction?(invalid_direction), do: false
+
+  defp valid_position?({a, b}), do: is_integer(a) and is_integer(b)
+  defp valid_position?(any), do: false
+
+  def simulate(robot, "") do
+    robot
+  end
+  def simulate(robot, "R") do
+    poles = [:north, :east, :south, :west]
+    index = Enum.find_index(poles, fn x -> x == direction(robot) end)
+    position = Enum.at(poles, index + 1)
+    %{
+        position: position,
+        direction: direction(robot)
+      }
+  end
+  def simulate(robot, "L") do
+    poles = [:north, :east, :south, :west]
+    index = Enum.find_index(poles, fn x -> x == direction(robot) end)
+    position = Enum.at(poles, index - 1)
+    %{
+        position: position,
+        direction: direction(robot)
+      }
+  end
+  def simulate(robot, "A") do
+  direction = direction(robot)
+  poles = [north: {1, 0}, east: {0, 1}, south: {-1, 0}, west: {0, -1}]
+  position = { 
+    position(robot)[0] + elem(poles[direction], 0) ,  position(robot)[1]  + elem(poles[direction], 1) 
+  }
+    robot
+      %{
+        position: position,
+        direction: direction
+      }
+  end
   @doc """
   Simulate the robot's movement given a string of instructions.
 
@@ -15,6 +68,9 @@ defmodule RobotSimulator do
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
+    instructions
+    |> String.split("")
+    |> Enum.reduce(robot, fn x, acc -> simulate(acc, x) end)
   end
 
   @doc """
@@ -24,6 +80,7 @@ defmodule RobotSimulator do
   """
   @spec direction(robot :: any) :: atom
   def direction(robot) do
+    Map.get(robot, :direction)
   end
 
   @doc """
@@ -31,5 +88,6 @@ defmodule RobotSimulator do
   """
   @spec position(robot :: any) :: {integer, integer}
   def position(robot) do
+    Map.get(robot, :position)
   end
 end
